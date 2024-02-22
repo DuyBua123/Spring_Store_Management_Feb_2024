51c91beb2c834be8f777cbd43d2a5a0d85ffb001
DuyBua <mondorea42@gmail.com>
2024-02-22 08:20:05 +0700

    Fresh spring project

---
+++ src/test/java/com/example/store_management_system_feb_2024/StoreManagementSystemFeb2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store_management_system_feb_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementSystemFeb2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
