fa89fd463f4ea3600e45ef227761d9cfa69f9a15
DuyBua <mondorea42@gmail.com>
2024-02-28 17:06:55 +0700

    Add user table

---
+++ src/main/java/com/example/store_management_system_feb_2024/models/User.java
@@ -0,0 +1,31 @@
+package com.example.store_management_system_feb_2024.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+    public Long id;
+
+    public String username;
+
+    public String email;
+
+    public String password;
+
+    public Date created_at;
+
+}
